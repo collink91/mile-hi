2b8383380acdbe8d566426aa34e2226b11f690fd
Jason <LutzJason92@gmail.com>
2021-12-16 13:55:58 -0700

    Models defined
    Modeled Sports and Outdoor Tables

---
+++ models/Outdoor.js
@@ -0,0 +1,39 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Outdoor extends Model {}
+
+Outdoor.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    type: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    where: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    group_activity: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+},
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "Outdoor",
+  }
+);
+
+module.exports = Outdoor;
---
+++ models/Sports.js
@@ -0,0 +1,39 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Sports extends Model {}
+
+Sports.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    type: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    where: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    group_activity: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+},
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "Sports",
+  }
+);
+
+module.exports = Sports;

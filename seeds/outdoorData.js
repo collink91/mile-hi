256ec7415051fa9320874893dcfa121530c86f9c
Jason <LutzJason92@gmail.com>
2021-12-16 17:17:24 -0700

    Update outdoorData.js
    Created Seed Data

--- seeds/outdoorData.js
+++ seeds/outdoorData.js
@@ -1,6 +1,21 @@
-const { Outdoor } = require("../models/Outdoor");
+const { Outdoor } = require("../models");
 
-const outdoorData = [{}];
+const outdoorData = [
+  {
+    type: "Hiking",
+    where: "Eldorado Canyon Sate Park",
+    group_activity: "false",
+    description:
+      "Crags Hotel Ruins and Continental divide Overlook. Prepare for a 3mile hike, ",
+  },
+  {
+    type: "Mountain Biking",
+    where: "Bear Creek Lake Regional Park",
+    group_activity: "false",
+    description:
+      "8miles of trail with great views. only 14miles from downtown. Bring Water!",
+  },
+];
 
 const seedOutdoor = () => Outdoor.bulkCreate(outdoorData);
 

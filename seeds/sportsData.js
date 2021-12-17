b15c493aacc7da127ff015fadef6385e641da7de
Jason <LutzJason92@gmail.com>
2021-12-16 17:26:38 -0700

    Update sportsData.js
    seed data created and succesfully seeds database

--- seeds/sportsData.js
+++ seeds/sportsData.js
@@ -1,6 +1,21 @@
-const { Sports } = require("../models/Sports");
+const { Sports } = require("../models");
 
-const sportsData = [{}];
+const sportsData = [
+  {
+    type: "Volley Ball",
+    where: "Cheesman Park",
+    group_activity: "true",
+    description:
+      "DU V CUBoulder, bring essentail oils and gluten free snacks, meet at noon this Saturday",
+  },
+  {
+    type: "Football",
+    where: "Washington Park",
+    group_activity: "true",
+    description:
+      "Looking for 2 other players to join the Denver Rec Center Football Team this sunday at 2pm",
+  },
+];
 
 const seedSports = () => Sports.bulkCreate(sportsData);
 

0fa94dae4849c9cca21ddd0efdc0dddaf63ece81
Jason <LutzJason92@gmail.com>
2021-12-17 19:48:23 -0700

    Update index.js
    added seed types functions

--- seeds/index.js
+++ seeds/index.js
@@ -1,26 +1,30 @@
 const sequelize = require("../config/connection");
 const seedOutdoor = require("./outdoorData");
 const seedSports = require("./sportsData");
+const seedUser = require("./userData");
 
 const seedEvents = require("./eventsData");
 const seedIndoor = require("./indoorData");
-const seedUser = require("./userData");
 
+const seedSportsType = require("./sportsTypeData");
+const seedOutdoorType = require("./outdoorTypeData");
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
 
+  await seedOutdoorType();
+
+  await seedSportsType();
+
   await seedOutdoor();
 
   await seedSports();
-
 
   await seedEvents();
 
   await seedIndoor();
 
   await seedUser();
-
 
   process.exit(0);
 };

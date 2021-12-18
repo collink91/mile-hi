b91c7f367ce6c2c98ff35e4109521a662e056b56
Jason <LutzJason92@gmail.com>
2021-12-17 19:46:17 -0700

    Update index.js
    included "Type" tables and created associations

--- models/index.js
+++ models/index.js
@@ -1,12 +1,12 @@
 const Outdoor = require("./Outdoor");
 const Sports = require("./Sports");
-
-const User = require("./User")
+const OutdoorType = require("./OutdoorType");
+const SportsType = require("./SportsType");
+const User = require("./User");
 const Indoor = require("./Indoor");
-const Events = require("./Events")
+const Events = require("./Events");
 
 // Key associations, if needed
-
 
 // Key associations
 /*
@@ -18,7 +18,23 @@ const Events = require("./Events")
   foreignKey: 'gallery_id',
 });
 */
+SportsType.hasMany(Sports, {
+  foreignKey: "id",
+  onDelete: "CASCADE",
+});
 
-module.exports = { Sports, Outdoor, User, Events, Indoor  };
+OutdoorType.hasMany(Outdoor, {
+  foreignKey: "id",
+  onDelete: "CASCADE",
+});
+
+module.exports = {
+  Sports,
+  SportsType,
+  Outdoor,
+  OutdoorType,
+  Events,
+  Indoor,
+  User,
+};
 // { [ADD BRACKETS], [xClass] };
-

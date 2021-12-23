8bb9db48613e7d7acae8022db88dc5d48a303fe3
Jason <LutzJason92@gmail.com>
2021-12-23 12:18:19 -0700

    Update index.js
    updated controller

--- controllers/index.js
+++ controllers/index.js
@@ -1,14 +1,14 @@
 const router = require("express").Router();
 
-const indoorHandler = 
+// const indoorHandler =
 const apiRoutes = require("./api");
 // All non-JSON-modified routes go here (i.e., direct links to each activities page)
 
-router.get('/outdoor', (req, res) => {
-    // Either import outdoorRoutes here or put it here directly
-})
+router.get("/outdoor", (req, res) => {
+  // Either import outdoorRoutes here or put it here directly
+});
 
-router.get('/indoor', indoorHandler)
+router.get("/indoor", indoorHandler);
 
 router.use("/api", apiRoutes);
 

ca70d92958d59406f6932c6018f820b2ecea9d9a
Jason <LutzJason92@gmail.com>
2021-12-23 12:17:54 -0700

    Routes updated
    updated to pass in where clause

--- controllers/sportsRoutes.js
+++ controllers/sportsRoutes.js
@@ -1,16 +1,20 @@
-const Sports = require("../models/Sports");
+const Activities = require("../models/Activities");
+const sportsRenderer = require("../views/sports.handlebars");
 
 // url lands @ http://localhost:3001/sports
 
 // GET all SPORTS activities
 module.exports = async (req, res) => {
   try {
-    const sportsData = await Sports.findAll();
-    if (!sportsData) {
-      res.status(404).json({ message: "No sports activities found" });
+    const sporstData = await Activities.find({
+      where: { keyword: "Sports" },
+    });
+    if (!sporstData) {
+      res.status(404).json({ message: "No Sports activities found" });
       return;
     }
-    res.status(200).json(sportsData);
+    // Renders for Handlebars
+    res.status(200).render("sports", sporstData);
   } catch (err) {
     res.status(500).json(err);
   }

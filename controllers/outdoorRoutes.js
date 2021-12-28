4943e4eb375e4298a8fcbebac63ee14c44d08a49
Lauren Buchholz <90865074+L-Buchholz@users.noreply.github.com>
2021-12-28 12:56:35 -0700

    Update to routes and handlebars
    Changed outdoorRoutes and outdoor.handlebars files to talk to one another using DB seeds. Front and back end unite!

--- controllers/outdoorRoutes.js
+++ controllers/outdoorRoutes.js
@@ -19,13 +19,17 @@ module.exports = async (req, res) => {
     const outdoorData = await Activities.findAll({
       where: { keyword: "Outdoor" },
     });
-    if (!outdoorData) {
+    const activities = outdoorData.map((activity) =>
+      activity.get({ plain: true })
+    );
+    if (!activities) {
       res.status(404).json({ message: "No Outdoor activities found" });
       return;
     }
     // Renders for Handlebars
-    res.status(200).render("outdoor", outdoorData);
+    res.status(200).render("outdoor", { activities });
   } catch (err) {
+    console.log(err);
     res.status(500).json(err);
   }
 };

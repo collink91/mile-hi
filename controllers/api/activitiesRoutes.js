2aa30c6a67ea66cb1804f7e8aadd8cf393c2f1d6
Lauren Buchholz <90865074+L-Buchholz@users.noreply.github.com>
2021-12-29 18:37:18 -0700

    A further update to routes and views
    Tweaked code and moved login.handlebars page (up one level) so activitiesRoutes could connect with it directly

--- controllers/api/activitiesRoutes.js
+++ controllers/api/activitiesRoutes.js
@@ -1,6 +1,5 @@
 const router = require("express").Router();
-// NOTE: Do we need "restore" here?
-const { Activities, restore } = require("../../models/Activities");
+const { Activities } = require("../../models/Activities");
 // This ensures all new activities are authenticated through user login
 const authUser = require("../../utils/auth");
 
@@ -22,34 +21,6 @@ router.post("/", authUser, async (req, res) => {
 });
 
 module.exports = authUser;
-
-// GET all Activities (GET)
-router.get("/", async (req, res) => {
-  try {
-    const activitiesData = await Activities.findAll();
-    if (!activitiesData) {
-      res.status(404).json({ message: "No Activities found in Database" });
-      return;
-    }
-    res.status(200).json(activitiesData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-// GET a specific Activity
-router.get("/:id", async (req, res) => {
-  try {
-    const activitiesData = await Activities.findByPk(req.params.id);
-    if (!activitiesData) {
-      res.status(404).json({ message: "No Activities with this id!" });
-      return;
-    }
-    res.status(200).json(activitiesData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
 
 // Updating and Deleting not required; keep in icebox for now
 
--- utils/auth.js
+++ utils/auth.js
@@ -1,6 +1,6 @@
 // Connects user authorization on activitiesRoutes.js
 const authUser = (req, res, next) => {
-  if (!req.session.userId) {
+  if (!req.session.loggedIn) {
     res.redirect("/login");
   } else {
     next();
